5bbce8d03839205f5997a39989fcf306b2411a34
3raphat <treeraphat234@gmail.com>
2023-12-16 01:13:52 +0700

    Refactor login and signup page layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,8 +7,8 @@ export default function LoginPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <div className="flex flex-col space-y-2 text-center">
-          <Database className="mx-auto h-8 w-8" />
+        <div className="flex flex-col items-center space-y-2 text-center">
+          <Database className="h-8 w-8" />
           <h1 className="text-3xl font-semibold">เข้าสู่ระบบ</h1>
           <Balancer className="mt-2 text-muted-foreground">
             กรุณาเข้าสู่ระบบเพื่อดำเนินการต่อ
