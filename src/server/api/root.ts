37dd5e1b1a5f46eb54244a129848a61e4a2c6e08
Theeraphat J. <96657413+3raphat@users.noreply.github.com>
2024-01-04 02:40:09 +0000

    chore: remove unused import

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,6 @@
 import { logRouter } from "@/server/api/routers/log"
 import { memberListRouter } from "@/server/api/routers/memberList"
 import { memberPendingRouter } from "@/server/api/routers/memberPending"
-import { postRouter } from "@/server/api/routers/post"
 import { userRouter } from "@/server/api/routers/user"
 import { createTRPCRouter } from "@/server/api/trpc"
 
@@ -11,7 +10,6 @@ import { createTRPCRouter } from "@/server/api/trpc"
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   user: userRouter,
   log: logRouter,
   memberPending: memberPendingRouter,
